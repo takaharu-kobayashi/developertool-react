14f76083c82fa8519c5fa85f8cc80559df5d00a4
Takaharu Kobayashi <takaharu_kobayashi@yahoo.com>
2023-11-26 23:59:30 +0900

    correct src.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// GitHubリポジトリ名をここに入れる
+const repoName = 'developertool-react';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/'
 })
